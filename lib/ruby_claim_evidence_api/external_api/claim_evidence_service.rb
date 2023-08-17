ba064bcfcaabae7101fb4a35b53b88226504f55f
Jonathan Hoang <trinhjoanthan.hoang@va.gov>
2023-08-17 16:11:54 -0500

    clean lint in base64url method

--- lib/ruby_claim_evidence_api/external_api/claim_evidence_service.rb
+++ lib/ruby_claim_evidence_api/external_api/claim_evidence_service.rb
@@ -132,8 +132,7 @@ module ExternalApi
         encoded_source = Base64.encode64(source)
         encoded_source = encoded_source.sub(/=+$/, '')
         encoded_source = encoded_source.tr('+', '-')
-        encoded_source = encoded_source.tr('/', '_')
-        encoded_source
+        encoded_source.tr('/', '_')
       end
 
       def aws_client
