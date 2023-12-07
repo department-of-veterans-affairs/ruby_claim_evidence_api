db4ea815e42107094a1a3622f51735f1dd550fc9
vinner57 <128258952+vinner57@users.noreply.github.com>
2023-12-07 12:38:54 -0500

    upload file support for evidence service

--- lib/ruby_claim_evidence_api/fakes/claim_evidence_service.rb
+++ lib/ruby_claim_evidence_api/fakes/claim_evidence_service.rb
@@ -1,27 +1,34 @@
-# frozen_string_literal: true
-
-require 'ruby_claim_evidence_api/external_api/response'
+require "ruby_claim_evidence_api/external_api/response"
 module Fakes
   class ClaimEvidenceService
-    JWT_TOKEN = ENV['CLAIM_EVIDENCE_JWT_TOKEN']
-    BASE_URL = ENV['CLAIM_EVIDENCE_API_URL']
-    SERVER = '/api/v1/rest'
-    DOCUMENT_TYPES_ENDPOINT = '/documenttypes'
+    ENV = {
+      "CLAIM_EVIDENCE_JWT_TOKEN" =>
+   "yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3NmQxOWE2OC03MDA1LTRhNjgtOWEzNC03MzEzZmI0MjMzNzMiLCJpYXQiOjE1MTYyMzkwMjIsImlzcyI6ImRldmVsb3BlclRlc3RpbmciLCJhcHBsaWNhdGlvbklEIjoiVkJNUy1VSSIsInVzZXJJRCI6ImNob3dhcl9zc3VwZXIiLCJzdGF0aW9uSUQiOiIzMTcifQ.PoKDvoVdAh9oNsRmn5SD-MYfEghNiyAQjNNrbRCnqbw",
+      "CLAIM_EVIDENCE_API_URL" => "https://vefs-claimevidence-uat.stage.bip.va.gov",
+      "DEVVPN_PROXY" => "http://aideusername:aidepassword@127.0.0.1:9443",
+      "SSL_CERT_FILE" => "/Users/razortalent/Projects/caseflow-setup/caseflow/lib/data/vbms-internal.client.vbms.aide.oit.va.gov.crt",
+      "CLAIM_EVIDENCE_KEY_FILE" => "/Users/razortalent/Projects/caseflow-setup/caseflow/lib/data/vbms-internal.client.vbms.aide.oit.va.gov.key",
+      "CLAIM_EVIDENCE_CERT_PASSPHRASE" => "vbmsclient"
+    }.freeze
+    JWT_TOKEN = ENV["CLAIM_EVIDENCE_JWT_TOKEN"]
+    BASE_URL = ENV["CLAIM_EVIDENCE_API_URL"]
+    SERVER = "/api/v1/rest"
+    DOCUMENT_TYPES_ENDPOINT = "/documenttypes"
     # this must start with http://
-    HTTP_PROXY = ENV['DEVVPN_PROXY']
-    CERT_LOCATION = ENV['SSL_CERT_FILE']
-    KEY_LOCATION = ENV['CLAIM_EVIDENCE_KEY_FILE']
-    CERT_PASSWORD = ENV['CLAIM_EVIDENCE_CERT_PASSPHRASE']
+    HTTP_PROXY = ENV["DEVVPN_PROXY"]
+    CERT_LOCATION = ENV["SSL_CERT_FILE"]
+    KEY_LOCATION = ENV["CLAIM_EVIDENCE_KEY_FILE"]
+    CERT_PASSWORD = ENV["CLAIM_EVIDENCE_CERT_PASSPHRASE"]
     HEADERS = {
-      "Content-Type": 'application/json',
-      "Accept": '*/*'
+      "Content-Type": "application/json",
+      "Accept": "*/*"
     }.freeze
     CREDENTIALS = Aws::Credentials.new(
-      ENV['AWS_ACCESS_KEY_ID'],
-      ENV['AWS_SECRET_ACCESS_KEY']
+      ENV["AWS_ACCESS_KEY_ID"],
+      ENV["AWS_SECRET_ACCESS_KEY"]
     )
-    REGION = ENV['AWS_DEFAULT_REGION']
-    AWS_COMPREHEND_SCORE = ENV['AWS_COMPREHEND_SCORE']
+    REGION = ENV["AWS_DEFAULT_REGION"]
+    AWS_COMPREHEND_SCORE = ENV["AWS_COMPREHEND_SCORE"]
 
     class << self
       def document_types_request
@@ -34,23 +41,55 @@ module Fakes
 
       def ocr_document_request(doc_uuid)
         {
-          headers: HEADERS.merge("Content-Type": 'application/x-www-form-urlencoded'),
+          headers: HEADERS.merge("Content-Type": "application/x-www-form-urlencoded"),
           endpoint: "/files/#{doc_uuid}/data/ocr",
           method: :get
         }
+      end
+
+      def upload_document_request(file_path:, content_name:)
+        {
+          headers: HEADERS,
+          endpoint: "/files",
+          method: :post,
+          body: {
+            file: file_path,
+            payload: {
+              contentName: content_name,
+              providerData: {
+                contentSource: "VISTA",
+                documentTypeId: 131,
+                dateVaReceivedDocument: "2020-02-20",
+                subject: "TestSubject",
+                contentions: %w[brokenLeg brokenArm],
+                alternativeDocumentTypeIds: [1, 2, 3],
+                veteranFirstName: "john",
+                actionable: false,
+                isInitialNewMail: false,
+                veteranMiddleName: "middleName",
+                veteranLastName: "lastName",
+                veteranSuffix: "III"
+              }
+            }
+          }
+        }
+      end
+
+      def upload_document_types(file:)
+        use_faraday(upload_document_request(file_path: file.path, content_name: file.original_filename)).body
       end
 
       def document_types
         response = if HTTP_PROXY
                      use_faraday(document_types_request)
                    else
-                     JSON.parse(IO.binread(File.join(Rails.root, 'lib', 'data', 'DOCUMENT_TYPES.json')))
+                     JSON.parse(IO.binread(File.join(Rails.root, "lib", "data", "DOCUMENT_TYPES.json")))
                    end
 
         if HTTP_PROXY
-          response.body['documentTypes']
+          response.body["documentTypes"]
         else
-          response['documentTypes']
+          response["documentTypes"]
         end
       end
 
@@ -58,13 +97,13 @@ module Fakes
         response = if HTTP_PROXY
                      use_faraday(document_types_request)
                    else
-                     JSON.parse(IO.binread(File.join(Rails.root, 'lib', 'data', 'DOCUMENT_TYPES.json')))
+                     JSON.parse(IO.binread(File.join(Rails.root, "lib", "data", "DOCUMENT_TYPES.json")))
                    end
 
         if HTTP_PROXY
-          response.body['alternativeDocumentTypes']
+          response.body["alternativeDocumentTypes"]
         else
-          response['alternativeDocumentTypes']
+          response["alternativeDocumentTypes"]
         end
       end
 
@@ -72,13 +111,13 @@ module Fakes
         response = if HTTP_PROXY
                      use_faraday(ocr_document_request(doc_uuid))
                    else
-                     JSON.parse(IO.binread(File.join(Rails.root, 'lib', 'data', 'OCR_DOCUMENT.json')))
+                     JSON.parse(IO.binread(File.join(Rails.root, "lib", "data", "OCR_DOCUMENT.json")))
                    end
 
         if HTTP_PROXY
-          response.body['currentVersion']['file']['text']
+          response.body["currentVersion"]["file"]["text"]
         else
-          response['currentVersion']['file']['text']
+          response["currentVersion"]["file"]["text"]
         end
       end
 
@@ -147,7 +186,7 @@ module Fakes
       def get_key_phrases(ocr_data, stub_response: false)
         key_phrase_parameters = {
           text: ocr_data,
-          language_code: 'en'
+          language_code: "en"
         }
         if stub_response == true
           aws_stub_client.detect_key_phrases(key_phrase_parameters).key_phrases
