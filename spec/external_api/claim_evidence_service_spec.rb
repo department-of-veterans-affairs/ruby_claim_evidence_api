cda2267f6e8b21d7a59681d2aa58176941f82340
Kevin Ma <magavakevin10@gmail.com>
2024-02-01 10:47:19 -0500

    Updated rspec

--- spec/external_api/claim_evidence_service_spec.rb
+++ spec/external_api/claim_evidence_service_spec.rb
@@ -311,6 +311,24 @@ describe ExternalApi::ClaimEvidenceService do
     before do
       subject::REGION ||= aws_region
       subject::AWS_COMPREHEND_SCORE ||= 0.95
+      stub_request(:put, 'http://169.254.169.254/latest/api/token')
+        .with(
+          headers: {
+            'Accept' => '*/*',
+            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'User-Agent' => 'aws-sdk-ruby2/2.11.632',
+            'X-Aws-Ec2-Metadata-Token-Ttl-Seconds' => '21600'
+          }
+        ).to_return(status: 200, body: '', headers: {})
+
+      stub_request(:get, 'http://169.254.169.254/latest/meta-data/iam/security-credentials/')
+        .with(
+          headers: {
+            'Accept' => '*/*',
+            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'User-Agent' => 'aws-sdk-ruby2/2.11.632'
+          }
+        ).to_return(status: 200, body: '', headers: {})
     end
     let(:ocr_data) { 'Some text string' }
     let(:stub_response) do
