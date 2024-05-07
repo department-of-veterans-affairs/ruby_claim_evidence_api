77b9cb867676eab745215717d6ff4d430477d39b
Alexander Smith <alex.smith.99@thunderyard.com>
2024-05-07 09:43:16 -0600

    Convert get_file_list Method Values to Args

--- lib/ruby_claim_evidence_api/external_api/veteran_file_fetcher.rb
+++ lib/ruby_claim_evidence_api/external_api/veteran_file_fetcher.rb
@@ -4,18 +4,18 @@ module ExternalApi
   # Fetches CE API documents for a given veteran
   class VeteranFileFetcher
     def fetch_veteran_file_list(veteran_file_number:)
-      get_file_list(veteran_file_number)
+      get_file_list(veteran_file_number: veteran_file_number)
     end
 
     private
 
-    def get_file_list(veteran_file_number)
+    def get_file_list(veteran_file_number:, query: {}, body: nil)
       ExternalApi::ClaimEvidenceService.send_ce_api_request(
         endpoint: file_folders_search_endpoint,
-        query: {},
+        query: query,
         headers: x_folder_uri_header(veteran_file_number),
         method: :post,
-        body: nil
+        body: body
       )
     end
 
