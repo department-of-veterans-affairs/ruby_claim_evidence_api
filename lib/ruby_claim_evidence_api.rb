2d94fdcdfc6f66a8c3a3fdf0a4bb1129f2abe3fb
Alexander Smith <alex.smith.99@thunderyard.com>
2024-05-14 12:00:07 -0600

    Fix Rubocop Violations

--- lib/ruby_claim_evidence_api.rb
+++ lib/ruby_claim_evidence_api.rb
@@ -2,9 +2,11 @@
 
 require 'pry'
 
+# Services
 require 'ruby_claim_evidence_api/external_api/claim_evidence_service'
-require 'ruby_claim_evidence_api/fakes/claim_evidence_service'
 
+# Fakes
+require 'ruby_claim_evidence_api/fakes/claim_evidence_service'
 require 'ruby_claim_evidence_api/fakes/mock_api_client'
 
 class RubyClaimEvidenceApi
