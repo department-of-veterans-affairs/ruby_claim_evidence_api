9ff385ca703355b0cdb40125b4fdad40a6108d98
Kevin Ma <magavakevin10@gmail.com>
2024-05-14 15:20:36 -0400

    Added require

--- lib/ruby_claim_evidence_api.rb
+++ lib/ruby_claim_evidence_api.rb
@@ -5,6 +5,7 @@ require 'pry'
 # Services
 require 'ruby_claim_evidence_api/external_api/claim_evidence_service'
 require 'ruby_claim_evidence_api/external_api/veteran_file_fetcher'
+require 'ruby_claim_evidence_api'
 
 # Fakes
 require 'ruby_claim_evidence_api/fakes/claim_evidence_service'
