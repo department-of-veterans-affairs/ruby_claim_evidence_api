0663e98fbf4fd6cc41d9d2cb5828992517ea91b2
Kevma50287 <104021955+Kevma50287@users.noreply.github.com>
2024-05-15 16:31:39 -0400

    Kev ma/appeals 45277 v3 (#57)
    * Added CE API call to getDocumentContent
    
    * Added spec
    
    * Add New Veteran File Fetch Service
    
    - Create new service for fetching veteran files.
    
    - Add method for fetching a list of veteran files.
    
    - Add specs to test new method.
    
    * Fix Misc. Issues
    
    - Fix incorrectly-removed gem version in gemspec.
    
    - DRY up spec.
    
    * added fakes
    
    * Convert get_file_list Method Values to Args
    
    * move files to VeteranFetcher
    
    * Make Misc. Gem Updates
    
    - Fix issue preventing new vet file service from being called.
    
    - Add fake for new vet file service.
    
    * Remove Unneeded require
    
    * Fix failing Fakes::VeteranFileFetcher spec
    
    * Added get document content api call and specs
    
    * Added string parser helper
    
    * Update method to return byte string
    
    * fix import
    
    * Updated specs; Added local pdf output
    
    * Refactor Fakes to not rely on Caseflow dependencies
    
    * Update Gem to Accept Configuration
    
    - Add init option to allow gem configuration.
    
    - Add classes to support new use_canned_api_responses config
    option.
    
    * Fix Rubocop Violations
    
    * Updates for gem config
    
    * Added require
    
    * Added require
    
    * Refactor claim evidence location
    
    * Resolve merge conflicts
    
    * Replaced with ApiBase configuration
    
    * Rubocop Linting updates
    
    * Linting updates
    
    * Correct SSL verification for fakes
    
    ---------
    
    Co-authored-by: Alexander Smith <alex.smith.99@thunderyard.com>

--- lib/ruby_claim_evidence_api/find_env_variables.rb
+++ lib/ruby_claim_evidence_api/find_env_variables.rb
@@ -1,66 +1,65 @@
 # frozen_string_literal: true
 
 class FindEnvVariables
-    def call
-      files.each_with_object([]) do |file, result|
-        result << EnvVariablesSearch.new(file: file, regex: env_variable_regex).call
-      end
-    end
-  
-    private
-    
-    def files
-      app_rb_files + app_erb_files + config_files + script_files + lib_files
-    end
-  
-    def app_rb_files
-      Dir.glob("app/**/*.rb")
-    end
-  
-    def app_erb_files
-      Dir.glob("app/views/**/*.erb")
-    end
-  
-    def config_files
-      Dir.glob("config/**/*.*")
-    end
-  
-    def lib_files
-      Dir.glob("lib/**/*.rb")
-    end
-  
-    def script_files
-      Dir.glob("scripts/**/*.*")
-    end
-  
-    def env_variable_regex
-      /ENV\[\".+\"\]/
+  def call
+    files.each_with_object([]) do |file, result|
+      result << EnvVariablesSearch.new(file: file, regex: env_variable_regex).call
     end
   end
-  
-  class EnvVariablesSearch
-    def initialize(file:, regex:)
-      @file = file
-      @regex = regex
-    end
-  
-    def call
-      File.open(file, "r").each_with_object([]) do |line, result|
-        line.match(regex) do |found|
-          result << found[0]
-        end
+
+  private
+
+  def files
+    app_rb_files + app_erb_files + config_files + script_files + lib_files
+  end
+
+  def app_rb_files
+    Dir.glob('app/**/*.rb')
+  end
+
+  def app_erb_files
+    Dir.glob('app/views/**/*.erb')
+  end
+
+  def config_files
+    Dir.glob('config/**/*.*')
+  end
+
+  def lib_files
+    Dir.glob('lib/**/*.rb')
+  end
+
+  def script_files
+    Dir.glob('scripts/**/*.*')
+  end
+
+  def env_variable_regex
+    /ENV\[".+"\]/
+  end
+end
+
+class EnvVariablesSearch
+  def initialize(file:, regex:)
+    @file = file
+    @regex = regex
+  end
+
+  def call
+    File.open(file, 'r').each_with_object([]) do |line, result|
+      line.match(regex) do |found|
+        result << found[0]
       end
     end
-  
-    private
-  
-    attr_reader :file, :regex
   end
-  
-  all_env_variables = FindEnvVariables.new.call.flatten.uniq
-  all_env_variables.map! { |env| env.split(" ").flatten }.flatten!
-  all_env_variables.reject! { |env| ['?', '||', 'if'].include?(env) && !env.include?('ENV["')}
-  all_env_variables.map! { |env|  env.gsub(/ENV\[\"/, '').gsub(/\"\]/, '') }.flatten!
-  
-  puts all_env_variables.uniq.sort
-  
+
+  private
+
+  attr_reader :file, :regex
+end
+
+all_env_variables = FindEnvVariables.new.call.flatten.uniq
+all_env_variables.map! { |env| env.split(' ').flatten }.flatten!
+all_env_variables.reject! { |env| ['?', '||', 'if'].include?(env) && !env.include?('ENV["') }
+all_env_variables.map! { |env| env.gsub(/ENV\["/, '').gsub(/"\]/, '') }.flatten!
+
+puts all_env_variables.uniq.sort
