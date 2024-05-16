9a8ca8d4c7dbe71f1b41c8add353ec2774a0f61b
Kevin Ma <magavakevin10@gmail.com>
2024-05-16 11:08:46 -0400

    Update String Parser and added Rspecs

--- lib/ruby_claim_evidence_api/helpers/string_parser.rb
+++ lib/ruby_claim_evidence_api/helpers/string_parser.rb
@@ -4,6 +4,6 @@
 module StringParser
   # Document id's in Caseflow are wrapped in curly braces - CE API requires the id string with no wrapping
   def parse_document_id(id)
-    id.to_s.match(/\{(.+?)}\}/) ? ::Regexp.last_match(1) : id
+    id.to_s.gsub(/^\{(.*)\}$/, '\1')
   end
 end
