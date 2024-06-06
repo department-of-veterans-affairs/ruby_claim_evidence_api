f832daf88b883e9f75ad65f20e15cf8278cfeb87
Kevin Ma <magavakevin10@gmail.com>
2024-06-06 14:12:38 -0400

    Update error logging and messaging

--- lib/ruby_claim_evidence_api/external_api/response.rb
+++ lib/ruby_claim_evidence_api/external_api/response.rb
@@ -77,10 +77,16 @@ module ExternalApi
     def error_message
       return 'No error message from ClaimEvidence' if body.nil? || body.empty?
 
-      if @uses_net_http == true
-        body['message']
-      else
-        body['message'] || body['messages'] || body['errors'][0]['message']
+      begin
+        if @uses_net_http == true
+          body['message']
+        else
+          # Possible error response shapes taken from Claim Evidence Swagger
+          body['message'] || body['messages'] || body['errors'][0]['message']
+        end
+      rescue StandardError => e
+        logger.error "Error accessing response body: #{body}"
+        "Encountered #{e} while attempting to access response body"
       end
     end
   end
