5b6ff43460130c8a0fa409cbeda16aa6cd8a9d10
Alexander Smith <alex.smith.99@thunderyard.com>
2024-07-18 11:43:47 -0600

    Fix Misc. Issues
    - Add missing requires for new files.
    
    - Rename model class for better clarity.

--- lib/ruby_claim_evidence_api/models/claim_evidence_file_update_payload.rb
+++ lib/ruby_claim_evidence_api/models/claim_evidence_file_update_payload.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Encapsulates all of the data required to perform a file update in the CE API
-class FileUpdatePayload
+class ClaimEvidenceFileUpdatePayload
   attr_accessor :date_va_received_document,
                 :document_type_id,
                 :file_content,
