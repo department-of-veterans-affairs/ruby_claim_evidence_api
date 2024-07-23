a9e193a0b7a0086678ea3be7b9cff7ac98c70bfb
Alexander Smith <alex.smith.99@thunderyard.com>
2024-07-23 15:12:33 -0600

    Update Specs

--- spec/external_api/veteran_file_updater_spec.rb
+++ spec/external_api/veteran_file_updater_spec.rb
@@ -4,7 +4,7 @@ require 'ruby_claim_evidence_api/api_base'
 require 'ruby_claim_evidence_api/external_api/claim_evidence_service'
 require 'ruby_claim_evidence_api/external_api/response'
 require 'ruby_claim_evidence_api/external_api/veteran_file_updater'
-require 'ruby_claim_evidence_api/models/file_update_payload'
+require 'ruby_claim_evidence_api/models/claim_evidence_file_update_payload'
 require './spec/external_api/spec_helper'
 
 describe ExternalApi::VeteranFileUpdater do
@@ -14,34 +14,17 @@ describe ExternalApi::VeteranFileUpdater do
 
   describe '#update_veteran_file' do
     it 'calls the API endpoint' do
-      expect(ExternalApi::ClaimEvidenceService).to receive(:send_ce_api_request)
+      expect(ExternalApi::ClaimEvidenceService).to receive(:update_document)
         .with(
-          endpoint: '/files/0003E65D-0000-C118-A964-CA1AEC2925E6',
-          query: {},
-          headers: {
-            "Content-Type": 'application/json',
-            "Accept": '*/*',
-            "X-Folder-URI": 'VETERAN:FILENUMBER:123456789'
-          },
-          method: :post,
-          body:{
-            payload: {
-              providerData: {
-                contentSource: '',
-                documentTypeId: '',
-                dateVaReceivedDocument: ''
-              }
-            },
-            file: ''
-          }
+          veteran_file_number: '123456789',
+          file_uuid: '0003E65D-0000-C118-A964-CA1AEC2925E6',
+          file_update_payload: instance_of(ClaimEvidenceFileUpdatePayload)
         ).and_return(mock_api_response)
-
-      file_update_payload = ClaimEvidenceFileUpdatePayload.new
 
       described.update_veteran_file(
         veteran_file_number: '123456789',
         file_uuid: '0003E65D-0000-C118-A964-CA1AEC2925E6',
-        file_update_payload: file_update_payload
+        file_update_payload: ClaimEvidenceFileUpdatePayload.new
       )
     end
   end
