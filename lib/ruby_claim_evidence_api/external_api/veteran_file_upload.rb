7283cb332462af1810280fd2bb860fc4bdea0172
Deepak <santhi.parakal@va.gov>
2024-07-29 15:31:12 -0400

    add more fields

--- lib/ruby_claim_evidence_api/external_api/veteran_file_upload.rb
+++ lib/ruby_claim_evidence_api/external_api/veteran_file_upload.rb
@@ -4,18 +4,20 @@ require 'ruby_claim_evidence_api/api_base'
 
 module ExternalApi
   # upload documents through CE API for a given veteran
-  class VeteranFileUpload 
-    def upload_veteran_file(file_path:, veteran_file_number:, doc_info:) 
-        payload = {
-          contentName: doc_info.content_name,
-          providerData: {
-            contentSource: doc_info.content_source,
-            documentTypeId: doc_info.document_type_id,
-            dateVaReceivedDocument: doc_info.date_va_received_document
-          }
+  class VeteranFileUpload < ApiBase
+    def upload_veteran_file(file_path:, veteran_file_number:, doc_info:)
+      payload = {
+        contentName: doc_info.content_name,
+        providerData: {
+          contentSource: doc_info.content_source,
+          documentTypeId: doc_info.document_type_id,
+          dateVaReceivedDocument: doc_info.date_va_received_document,
+          subject: doc_info.subject,
+          newMail: doc_info.new_mail
         }
+      }
 
-    ExternalApi::ClaimEvidenceService.upload_document(file_path, veteran_file_number,payload)
+      api_client.upload_document(file_path, veteran_file_number, payload)
     end
   end
 end
