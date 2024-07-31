64d4860598219b174f08e2d45bedb89bc69ae2d3
Deepak <santhi.parakal@va.gov>
2024-07-31 09:57:30 -0400

    require files

--- lib/ruby_claim_evidence_api.rb
+++ lib/ruby_claim_evidence_api.rb
@@ -7,9 +7,11 @@ require 'ruby_claim_evidence_api/api_base'
 require 'ruby_claim_evidence_api/external_api/claim_evidence_service'
 require 'ruby_claim_evidence_api/external_api/veteran_file_fetcher'
 require 'ruby_claim_evidence_api/external_api/veteran_file_updater'
+require 'ruby_claim_evidence_api/external_api/veteran_file_upload'
 
 # Models
 require 'ruby_claim_evidence_api/models/claim_evidence_file_update_payload'
+require 'ruby_claim_evidence_api/models/claim_evidence_file_upload_payload'
 
 # Fakes
 require 'ruby_claim_evidence_api/fakes/claim_evidence_service'
